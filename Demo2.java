3a078818ea1dc5b55b83ad168bfb6bd31e0a576e
Brajul-Rajput <brajulrajput@gmail.com>
2024-06-26 11:37:22 +0530

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,16 @@
+class pen{
+    String color;   /*DATA */
+String type;
+
+public void show(){
+    System.out.println("BLACK COLOR BALL PEN");      /*METHOD */
+}
+}
+public class Demo2 {
+    public static void main(String []args){
+pen pen1=new pen();
+pen1.color="black";
+pen1.type="ball";
+pen1.show();
+    }
+}
