7cc53483e456a82fa55679b3f580761602d91417
Brajul-Rajput <brajulrajput@gmail.com>
2024-07-16 10:04:36 +0530

    Create in.java

---
+++ in.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class in{
+    public static void main(String[]args){
+        int a;
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the value in input :");
+         a=sc.nextInt();
+        System.out.println( "value :" +a);
+    }
+}
